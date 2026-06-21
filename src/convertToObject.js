d984902aeb2e914e6fa3a20bc13a098475ad7d39
Margaret <margarita@medentee.com>
2026-06-21 11:54:03 +0300

    <task final version>

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
-}
+	return sourceString.split(';').reduce((obj, declaration) => {
+		const colonIndex = declaration.indexOf(':');
 
+		if (colonIndex === -1) {
+			return obj;
+		}
+
+		const key = declaration.slice(0, colonIndex).replace(/\s+/g, '');
+		const value = declaration.slice(colonIndex + 1).trim();
+
+		if (!key) {
+			return obj;
+		}
+
+		obj[key] = value;
+
+		return obj;
+	}, {});
+}
 module.exports = convertToObject;
+
+  
